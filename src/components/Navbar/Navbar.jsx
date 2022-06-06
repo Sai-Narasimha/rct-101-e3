3bb46de3dc0761d4d5c8b6585ed9834c25b18f6e
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2022-06-06 16:52:55 +0530

    final commit before 5pm

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 // use react-router Link or NavLink
-const Link = <a />;
+// const Link = <a />;
+import { AuthContext } from "../../context/AuthContext";
+import {useContext} from 'react';
 
 const Navbar = () => {
+  const {isAuth} = useContext(AuthContext)
   return (
-    <div data-cy="navbar">
-      <Link data-cy="navbar-home-link"></Link>
-      <span data-cy="navbar-cart-items-count">{/* count here */}</span>
-      <button data-cy="navbar-login-logout-button"></button>
+    <div data-cy="navbar" className="navbar">
+      <Link data-cy="navbar-home-link" to="">
+        logo
+      </Link>
+      <div className="cart">
+        <span data-cy="navbar-cart-items-count">Cart : {0}</span>
+        <button data-cy="navbar-login-logout-button">{isAuth?"Logout" : "Login"}</button>
+      </div>
     </div>
   );
 };
